21972a36722199e7b80f274beb216eaf58daad92
carrybanana <maomaoyeah520@163.com>
2024-04-04 23:20:48 +0800

    for whlie

---
+++ C++PrimerPlus/5.15/dowhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int n;
+
+    cout << "Enter the numbers in the range 1-10 to find ";
+    cout << "my favorite number\n";
+    do{
+        cin >> n;
+    
+    }while (n != 7);
+    cout << "Yes, 7 is my favorite number.\n";
+
+    return 0;
+}
